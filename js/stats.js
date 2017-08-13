2b8f8ed979a2f508424f4e11470c34ddefa71dc1
Andrey Ostrovsky <andruxa0286@gmail.com>
2017-08-13 23:16:28 +0300

    Исправил оши

--- js/stats.js
+++ js/stats.js
@@ -1,45 +1,53 @@
 'use strict';
 
 window.renderStatistics = function (ctx, names, times) {
+  var max = -1;
+
+  // coordinates and sizes
+  var INITIAL_X = 120;
+  var INITIAL_Y = 250;
+  var BAR_WIDTH = 40;
+
+  // indents
+  var INDENT = 90;
+  var NAME_INDENT = 20;
+  var TIME_INDENT = 5;
+
+  // shadow rectangle
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 420, 270);
+
+  // white rectangle
   ctx.strokeRect(100, 10, 420, 270);
-  ctx.fillStyle = 'white';
+  ctx.fillStyle = 'rgba(255, 255, 255, 1)';
   ctx.fillRect(100, 10, 420, 270);
-  ctx.fillStyle = '#000000';
+
+  // text
+  ctx.fillStyle = 'rgba(0, 0, 0, 1)';
   ctx.font = '14px PT Mono';
   ctx.fillText('Ура вы победили!', 120, 40);
 
-  var max = -1;
-  var barWidht = 40;
-  var indent = 90;
-  var initialX = 120;
-  var initialY = 250;
-  var nameIndent = 20;
-  var timeIndent = 5;
-  var startAlpha = 0.4;
+  function generateColor() {
+    var color = (name === 'Вы') ? 'rgba(255, 0, 0, 1)' : 'rgba(0, 0, 255,' + (Math.random()) + ')';
+    return color;
+  }
 
   ctx.textBaseline = 'bottom';
   for (var i = 0; i < times.length; i++) {
     var time = times[i];
+    var name = names[i];
+    var histogramHeight = 150;
+
     if (time > max) {
       max = time;
     }
 
-    var histogramHeight = 150;
     var step = histogramHeight / max;
-    var name = names[i];
-    var decimal = i / 10;
 
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      ctx.fillStyle = 'rgba(0, 0, 255,' + (startAlpha + decimal) + ')';
-    }
-
-    ctx.fillRect(initialX + indent * i, initialY, barWidht, -times[i] * step);
-    ctx.fillStyle = '#000000';
-    ctx.fillText(name, initialX + indent * i, initialY + nameIndent);
-    ctx.fillText(time.toFixed(0), initialX + indent * i, initialY - times[i] * step - timeIndent);
+    ctx.fillStyle = generateColor();
+    ctx.fillRect(INITIAL_X + INDENT * i, INITIAL_Y, BAR_WIDTH, -times[i] * step);
+    ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+    ctx.fillText(name, INITIAL_X + INDENT * i, INITIAL_Y + NAME_INDENT);
+    ctx.fillText(time.toFixed(0), INITIAL_X + INDENT * i, INITIAL_Y - times[i] * step - TIME_INDENT);
   }
 };

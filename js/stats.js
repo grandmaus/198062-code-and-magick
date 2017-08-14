3dac7544d95d1f2ef2993b37c40bbab71b87394d
Andrey Ostrovsky <andruxa0286@gmail.com>
2017-08-14 22:29:51 +0300

    function для случайного цвета, комментарии к расчёту высот элементов гистограммы

--- js/stats.js
+++ js/stats.js
@@ -27,9 +27,8 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.font = '14px PT Mono';
   ctx.fillText('Ура вы победили!', 120, 40);
 
-  function generateColor() {
-    var color = (name === 'Вы') ? 'rgba(255, 0, 0, 1)' : 'rgba(0, 0, 255,' + (Math.random()) + ')';
-    return color;
+  function randomBlue() {
+    ctx.fillStyle = 'rgba(0, 0, 255,' + (Math.random()) + ')';
   }
 
   ctx.textBaseline = 'bottom';
@@ -44,10 +43,26 @@ window.renderStatistics = function (ctx, names, times) {
 
     var step = histogramHeight / max;
 
-    ctx.fillStyle = generateColor();
-    ctx.fillRect(INITIAL_X + INDENT * i, INITIAL_Y, BAR_WIDTH, -times[i] * step);
+    var color = (name === 'Вы') ? 'rgba(255, 0, 0, 1)' : randomBlue();
+    ctx.fillStyle = color;
+    /* INITIAL_X -- начальная координата по оси x;
+    INITIAL_X + INDENT * i -- к начальной точке прибавляем значение переменной
+    INDENT, умноженной на индекс элемента массива для смещения каждой последующей колонки вправо;
+    INITIAL_Y -- начальная координата по оси x;
+    BAR_WIDTH -- ширина колонки;
+    -times * step -- для определения высоты элемента гистограммы считается пропорция, в которой
+    step это результат деления 100%(histogramHeight) на максимальное значение(max), а time это время текущего игрока
+    Унарный минус перед time нужен для того, чтобы увеличение  прямоугольника происходило не вниз, а вверх */
+    ctx.fillRect(INITIAL_X + INDENT * i, INITIAL_Y, BAR_WIDTH, -time * step);
     ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+    /* name - имя текущего игрока;
+    значение координаты по x высчитывается так же, как у колонок;
+    INITIAL_Y + NAME_INDENT -- значение по оси y. Равно начальной координате колонки плюс отступ вниз; */
     ctx.fillText(name, INITIAL_X + INDENT * i, INITIAL_Y + NAME_INDENT);
-    ctx.fillText(time.toFixed(0), INITIAL_X + INDENT * i, INITIAL_Y - times[i] * step - TIME_INDENT);
+    /* time.toFixed(0) - время текущего игрока, округлённое до целой части;
+    значение координаты по x высчитывается так же, как у колонок;
+    INITIAL_Y - time * step - TIME_INDENT -- значение по оси y. Равно начальной координате колонки с
+    отступом равным высоте колонки(time * step) и дополнительным отступом от края колонки(TIME_INDENT); */
+    ctx.fillText(time.toFixed(0), INITIAL_X + INDENT * i, INITIAL_Y - time * step - TIME_INDENT);
   }
 };

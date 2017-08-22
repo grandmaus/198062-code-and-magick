8ecdb18d8c106127604fbdcc56ab6d36a96d95d6
Andrey Ostrovsky <andruxa0286@gmail.com>
2017-08-22 23:02:03 +0300

    Изменил названия переменных, отформатировал код

--- js/setup.js
+++ js/setup.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // объект с массивами характеристик персонажей
-var WIZARD_CHARACTERISTICS = {
-  names: [
+var WizardParams = {
+  Names: [
     'Иван',
     'Хуан Себастьян',
     'Мария', 'Кристоф',
@@ -11,7 +11,7 @@ var WIZARD_CHARACTERISTICS = {
     'Люпита',
     'Вашингтон'
   ],
-  surnames: [
+  Surnames: [
     'да Марья',
     'Верон',
     'Мирабелла',
@@ -21,7 +21,7 @@ var WIZARD_CHARACTERISTICS = {
     'Нионго',
     'Ирвинг'
   ],
-  coatColors: [
+  CoatColors: [
     'rgb(101, 137, 164)',
     'rgb(241, 43, 107)',
     'rgb(146, 100, 161)',
@@ -29,7 +29,7 @@ var WIZARD_CHARACTERISTICS = {
     'rgb(215, 210, 55)',
     'rgb(0, 0, 0)'
   ],
-  eyesColors: [
+  EyesColors: [
     'black',
     'red',
     'blue',
@@ -37,17 +37,19 @@ var WIZARD_CHARACTERISTICS = {
     'green'
   ]
 };
+
 // количество магов
 var WIZARDS_COUNT = 4;
 
 var popupSetup = document.querySelector('.setup');
+
 // переменные для вывода персонажей на страницу
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
 
 // функция возвращает случайное целое число
 var getRandomNumber = function (min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+  return Math.floor(Math.random() * (max - min));
 };
 
 // функция возвращает случайный элемент массива. В параметр передаётся имя массива
@@ -60,9 +62,9 @@ var getRandomArrayElement = function (array) {
 // функция возвращает сгенерированный объект персонажа
 var getRandomWizard = function () {
   var wizardObject = {
-    name: getRandomArrayElement(WIZARD_CHARACTERISTICS.names) + ' ' + getRandomArrayElement(WIZARD_CHARACTERISTICS.surnames),
-    coatColor: getRandomArrayElement(WIZARD_CHARACTERISTICS.coatColors),
-    eyesColor: getRandomArrayElement(WIZARD_CHARACTERISTICS.eyesColors)
+    name: getRandomArrayElement(WizardParams.Names) + ' ' + getRandomArrayElement(WizardParams.Surnames),
+    coatColor: getRandomArrayElement(WizardParams.CoatColors),
+    eyesColor: getRandomArrayElement(WizardParams.EyesColors)
   };
   return wizardObject;
 };
@@ -77,17 +79,20 @@ var renderWizard = function (wizard) {
   return wizardElement;
 };
 
-// функция возвращает fragment с персонажами
+// функция возвращает фрагмент с DOM нодами персонажей
 var createWizardFragment = function () {
   var fragment = document.createDocumentFragment();
   var randomWizard;
+
   for (var i = 0; i < WIZARDS_COUNT; i++) {
     randomWizard = getRandomWizard();
     fragment.appendChild(renderWizard(randomWizard));
   }
+
   return fragment;
 };
 
+// вставляем фрагмент с персонажами на страницу
 similarListElement.appendChild(createWizardFragment());
 popupSetup.classList.remove('hidden');
 popupSetup.querySelector('.setup-similar').classList.remove('hidden');
